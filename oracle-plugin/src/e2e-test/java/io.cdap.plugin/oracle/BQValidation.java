c2af653bd8b4c101b34c0e8c47b4cfc987ac7c26
bharatgulati <bharat.gulati@cloudsufi.com>
2023-05-15 18:47:16 +0530

    BQValidation for Oracle

--- oracle-plugin/src/e2e-test/java/io.cdap.plugin/oracle/BQValidation.java
+++ oracle-plugin/src/e2e-test/java/io.cdap.plugin/oracle/BQValidation.java
@@ -1,109 +1,50 @@
-/*
- * Copyright © 2023 Cask Data, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package io.cdap.plugin.oracle;
 
 import com.google.cloud.bigquery.TableResult;
-import com.google.common.collect.MapDifference;
-import com.google.common.collect.Maps;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
 import io.cdap.e2e.utils.BigQueryClient;
 import io.cdap.e2e.utils.PluginPropertyUtils;
 import io.cdap.plugin.OracleClient;
+import org.junit.Assert;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
-/**
- *  Big Query client
- */
 public class BQValidation {
 
-  /**
-   * Extracts entire data from source and target tables.
-   * @param sourceTable table at the source side
-   * @param targetTable table at the BigQuery side
-   * @return true if the values in source and target side are equal
-   */
-  public static void validateBQAndDBRecordValues(String schema, String sourceTable, String targetTable)
-    throws SQLException, ClassNotFoundException, IOException, InterruptedException {
+  public static boolean validateBQAndDBRecordValues(String schema, String sourceTable, String targetTable)
+    throws SQLException, ClassNotFoundException, ParseException, IOException, InterruptedException {
+    List<JsonObject> jsonObject = new ArrayList<>();
+    List<Object> list = new ArrayList<>();
+    getBigQueryTableData(targetTable, list);
+    for (Object jsonRow : list) {
+      JsonObject json = new Gson().fromJson(String.valueOf(jsonRow), JsonObject.class);
+      jsonObject.add(json);
+    }
     String getSourceQuery = "SELECT * FROM " + schema + "." + sourceTable;
-    List<Object> bigQueryRows = new ArrayList<Object>();
-
     try (Connection connect = OracleClient.getOracleConnection()) {
       connect.setHoldability(ResultSet.HOLD_CURSORS_OVER_COMMIT);
       Statement statement1 = connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE,
                                                      ResultSet.HOLD_CURSORS_OVER_COMMIT);
 
       ResultSet rsSource = statement1.executeQuery(getSourceQuery);
-      getBigQueryTableData(targetTable, bigQueryRows);
-
-
-      System.out.println(convertResultSetToJson(rsSource));
-      System.out.println(bigQueryRows.get(0));
-      //return OracleClient.compareResultSetData(rsSource, rsTarget);
+      return compareResultSetData(rsSource, jsonObject);
     }
   }
 
-  public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException, InterruptedException {
-    //Bytes and string - BQ - E2E_TARGET_3deae021_18ac_437f_8ea6_eb0ac725ffad
-    //oracle - SOURCETABLE_UHTTOKLBCU
-
-    //validateBQAndDBRecordValues("HR", "SOURCETABLE_IDGXDINEUX", "TestSN_tablehHI8cg1Qy3");
-    validateBQAndDBRecordValues("HR", "SOURCETABLE_UHTTOKLBCU", "E2E_TARGET_3deae021_18ac_437f_8ea6_eb0ac725ffad");
-  }
-
-  public static String convertResultSetToJson(ResultSet rs) throws SQLException {
-    List<JsonObject> jsonObjects = new ArrayList<>();
-    while (rs.next()) {
-      int numColumns = rs.getMetaData().getColumnCount();
-      JsonObject jsonObject = new JsonObject();
-      for (int i = 1; i <= numColumns; i++) {
-        String column_name = rs.getMetaData().getColumnName(i);
-        Object column_value = rs.getObject(i);
-        if (column_value == null) {
-          jsonObject.addProperty(column_name, "");
-        } else {
-          jsonObject.addProperty(column_name, column_value.toString());
-        }
-      }
-      jsonObjects.add(jsonObject);
-    }
-
-    JsonArray jsonArray = new JsonArray();
-    for (JsonObject jsonObject : jsonObjects) {
-      jsonArray.add(jsonObject);
-    }
-
-    // Convert JSON array to JSON string
-    Gson gson = new Gson();
-    String jsonString = gson.toJson(jsonArray);
-
-    return jsonString;
-  }
 
   private static void getBigQueryTableData(String table, List<Object> bigQueryRows)
     throws IOException, InterruptedException {
@@ -111,19 +52,95 @@ public class BQValidation {
     String projectId = PluginPropertyUtils.pluginProp("projectId");
     String dataset = PluginPropertyUtils.pluginProp("dataset");
     String selectQuery = "SELECT TO_JSON(t) FROM `" + projectId + "." + dataset + "." + table + "` AS t";
-    System.out.println(selectQuery);
     TableResult result = BigQueryClient.getQueryResult(selectQuery);
     result.iterateAll().forEach(value -> bigQueryRows.add(value.get(0).getValue()));
   }
 
-  private static boolean compareValueOfBothResponses(String sfmcResponse, String bigQueryResponse) {
-    Gson gson = new Gson();
-    Type type = new TypeToken<Map<String, Object>>() {
-    }.getType();
-    Map<String, Object> sfmcResponseInmap = gson.fromJson(sfmcResponse, type);
-    Map<String, Object> bigQueryResponseInMap = gson.fromJson(bigQueryResponse, type);
-    MapDifference<String, Object> mapDifference = Maps.difference(sfmcResponseInmap, bigQueryResponseInMap);
+  public static boolean compareResultSetData(ResultSet rsSource, List<JsonObject> bigQueryData) throws SQLException,
+    ParseException {
+    ResultSetMetaData mdSource = rsSource.getMetaData();
+    boolean result = false;
+    int columnCountSource = mdSource.getColumnCount();
 
-    return mapDifference.areEqual();
+    if (bigQueryData == null) {
+      Assert.fail("bigQueryData is null");
+      return result;
+    }
+
+    // Get the column count of the first JsonObject in bigQueryData
+    int columnCountTarget = 0;
+    if (bigQueryData.size() > 0) {
+      columnCountTarget = bigQueryData.get(0).entrySet().size();
+    }
+    // Compare the number of columns in the source and target
+    Assert.assertEquals("Number of columns in source and target are not equal",
+                        columnCountSource, columnCountTarget);
+
+    //Variable 'jsonObjectIdx' to track the index of the current JsonObject in the bigQueryData list,
+    int jsonObjectIdx = 0;
+    while (rsSource.next()) {
+      int currentColumnCount = 1;
+      while (currentColumnCount <= columnCountSource) {
+        String columnTypeName = mdSource.getColumnTypeName(currentColumnCount);
+        int columnType = mdSource.getColumnType(currentColumnCount);
+        String columnName = mdSource.getColumnName(currentColumnCount);
+        // Perform different comparisons based on column type
+        switch (columnType) {
+          // Since we skip BFILE in Oracle Sink, we are not comparing the BFILE source and sink values
+          case OracleSourceSchemaReader.BFILE:
+            break;
+          case Types.BLOB:
+          case Types.VARBINARY:
+          case Types.LONGVARBINARY:
+            String sourceB64String = new String(Base64.getEncoder().encode(rsSource.getBytes(currentColumnCount)));
+            String targetB64String = bigQueryData.get(jsonObjectIdx).get(columnName).getAsString();
+            Assert.assertEquals("Different values found for column : %s",
+                                sourceB64String, targetB64String);
+            break;
+
+          case Types.NUMERIC:
+            long sourceVal = rsSource.getLong(currentColumnCount);
+            long targetVal = Long.parseLong(bigQueryData.get(jsonObjectIdx).get(columnName).getAsString());
+            Assert.assertTrue("Different values found for column : %s",
+                              String.valueOf(sourceVal).equals(String.valueOf(targetVal)));
+            break;
+
+          case Types.TIMESTAMP:
+            Timestamp sourceTS = rsSource.getTimestamp(columnName);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+            Date parsedDate = dateFormat.parse(bigQueryData.get(jsonObjectIdx).get(columnName).getAsString());
+            Timestamp targetTs = new java.sql.Timestamp(parsedDate.getTime());
+            result = sourceTS.equals(targetTs);
+            Assert.assertEquals("Different values found for column : %s", result);
+            break;
+          case OracleSourceSchemaReader.BINARY_FLOAT:
+            Float sourceBytes = rsSource.getFloat(currentColumnCount);
+            Float targetBytes = bigQueryData.get(jsonObjectIdx).get(columnName).getAsFloat();
+            Assert.assertTrue(String.format("Different values found for column : %s", columnName),
+                              Float.compare(sourceBytes, targetBytes) == 0);
+            break;
+
+          case OracleSourceSchemaReader.BINARY_DOUBLE:
+            double sourceVAL = rsSource.getDouble(currentColumnCount);
+            double targetVAL = bigQueryData.get(jsonObjectIdx).get(columnName).getAsDouble();
+            Assert.assertTrue(String.format("Different values found for column : %s", columnName),
+                              Double.compare(sourceVAL, targetVAL) == 0);
+            break;
+
+          default:
+            String sourceString = rsSource.getString(currentColumnCount);
+            String targetString = bigQueryData.get(jsonObjectIdx).get(columnName).getAsString();
+            Assert.assertEquals(String.format("Different %s values found for column : %s", columnTypeName, columnName),
+                                String.valueOf(sourceString), String.valueOf(targetString));
+        }
+        currentColumnCount++;
+      }
+      jsonObjectIdx++;
+    }
+    Assert.assertFalse("Number of rows in Source table is greater than the number of rows in Target table",
+                       rsSource.next());
+//    Assert.assertFalse("Number of rows in Target table is greater than the number of rows in Source table",
+//                       rsTarget.next());
+    return true;
   }
 }
