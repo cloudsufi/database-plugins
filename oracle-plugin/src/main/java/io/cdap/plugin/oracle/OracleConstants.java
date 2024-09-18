609f90893cdf8b4f33180c36dae828e3f456a82f
vipinbhatt <vipin.bhatt@cloudsufi.com>
2024-09-18 15:12:37 +0530

    Revert "Add support for TCPS based connection string to support SSL/TLS connection."
    This reverts commit 874451cc7b75851b4046d14fb486a003864e2b95.

--- oracle-plugin/src/main/java/io/cdap/plugin/oracle/OracleConstants.java
+++ oracle-plugin/src/main/java/io/cdap/plugin/oracle/OracleConstants.java
@@ -30,7 +30,6 @@ public final class OracleConstants {
   public static final String ORACLE_CONNECTION_STRING_SID_FORMAT = "jdbc:oracle:thin:@%s:%s:%s";
   public static final String ORACLE_CONNECTION_STRING_SERVICE_NAME_FORMAT = "jdbc:oracle:thin:@//%s:%s/%s";
   public static final String ORACLE_CONNECTION_STRING_TNS_FORMAT = "jdbc:oracle:thin:@%s";
-  public static final String ORACLE_CONNECTION_STRING_TCPS_FORMAT = "jdbc:oracle:thin:@tcps://%s:%s/%s";
   public static final String DEFAULT_BATCH_VALUE = "defaultBatchValue";
   public static final String DEFAULT_ROW_PREFETCH = "defaultRowPrefetch";
   public static final String SERVICE_CONNECTION_TYPE = "service";
@@ -38,13 +37,12 @@ public final class OracleConstants {
   public static final String ROLE = "role";
   public static final String NAME_DATABASE = "database";
   public static final String TNS_CONNECTION_TYPE = "TNS";
-  public static final String TCPS_CONNECTION_TYPE = "TCPS";
   public static final String TRANSACTION_ISOLATION_LEVEL = "transactionIsolationLevel";
 
   /**
    * Returns the Connection String for the given ConnectionType.
    *
-   * @param connectionType TNS/Service/TCPS/SID
+   * @param connectionType TNS/Service/SID
    * @param host Host name of the oracle server
    * @param port Port of the oracle server
    * @param database Database to connect to
@@ -60,10 +58,6 @@ public final class OracleConstants {
     if (OracleConstants.SERVICE_CONNECTION_TYPE.equalsIgnoreCase(connectionType)) {
       return String.format(OracleConstants.ORACLE_CONNECTION_STRING_SERVICE_NAME_FORMAT,
           host, port, database);
-    }
-    if (OracleConstants.TCPS_CONNECTION_TYPE.equalsIgnoreCase(connectionType)) {
-      return String.format(OracleConstants.ORACLE_CONNECTION_STRING_TCPS_FORMAT,
-              host, port, database);
     }
     return String.format(OracleConstants.ORACLE_CONNECTION_STRING_SID_FORMAT,
         host, port, database);
